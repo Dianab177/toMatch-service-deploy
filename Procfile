5f8b4e9525c00a0ba1ead338661cbf31002cbc6c
DianaB177 <anaidb77@gmail.com>
2024-07-14 23:44:48 +0200

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: php artisan serve --host=0.0.0.0 --port=$PORT
+
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,14 +37,14 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\CorsMiddleware::class, // Añadir esta línea
+            \App\Http\Middleware\CorsMiddleware::class,
         ],
     
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\CorsMiddleware::class, // Añadir esta línea
+            \App\Http\Middleware\CorsMiddleware::class,
         ],
     ];
 
